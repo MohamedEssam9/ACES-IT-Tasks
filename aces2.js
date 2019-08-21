16c5813fbb6cfed24f49df608261e447d90cf69b
MohamedEssam9 <36486932+MohamedEssam9@users.noreply.github.com>
2019-08-21 18:23:45 +0200

    Add files via upload

---
+++ aces2.js
@@ -0,0 +1,4 @@
+var bu=document.getElementById("oneButton");
+document.addEventListener("click", function(){
+   document.getElementById("Model").style.backgroundColor=document.getElementById("abc").value;
+});
